0c1ab6252b0ac447553058aaacd902b86ba065f7
mili Arsema <meleymm88@gmail.com>
2026-04-10 11:22:00 -0500

    Logged data types

--- src/main/java/com/pluralsight/DataTypes.java
+++ src/main/java/com/pluralsight/DataTypes.java
@@ -1,4 +1,25 @@
 package com.pluralsight;
-
+// name of program
 public class DataTypes {
+    //main: starting point, {}: where code goes
+    public static void main(String[] args) {
+
+        //whole number
+        int
+
+        //decimal
+        float
+        double
+
+        //Text
+        String
+
+        //true/falso
+        boolean
+
+        //single letter
+        char
+
+    }
+
 }
